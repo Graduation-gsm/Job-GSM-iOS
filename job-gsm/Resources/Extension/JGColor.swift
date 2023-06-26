13703178d1e0fec6ef0e3aaa25ebc35229f5a283
minaje0917 <k050918@naver.com>
2023-06-26 20:13:01 +0900

    :lipstick: UI :: Add JG Color

---
+++ job-gsm/Resources/Extension/JGColor.swift
@@ -0,0 +1,5 @@
+import UIKit
+
+extension UIColor {
+    static var main = UIColor(named: "Main")
+}
--- job-gsm/Sources/Presentation/OnBoarding/View/OnBoardingViewController.swift
+++ job-gsm/Sources/Presentation/OnBoarding/View/OnBoardingViewController.swift
@@ -6,24 +6,59 @@
 //
 
 import UIKit
+import SnapKit
+import Then
 
-class OnBoardingViewController: UIViewController {
-
+class OnBoardingViewController: BaseViewController<OnBoardingViewModel> {
+    let vm = OnBoardingViewModel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    init(){
+        super.init(vm)
     }
-    */
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private let logo = UIImageView().then {
+        $0.image = UIImage(named: "OnBoardingLogo.svg")
+    }
+    
+    private let subText = UILabel().then {
+        $0.text = "GSM의 채용 공고를\n간편하게 확인해보세요"
+        $0.textColor = UIColor(
+            red: 67/255,
+            green: 142/255,
+            blue: 1,
+            alpha: 0.6
+        )
+        $0.font = UIFont.JGFont(
+            size: 15,
+            family: .Regular
+        )
+        $0.numberOfLines = 2
+        $0.textAlignment = .center
+    }
+    
+    override func addView() {
+        [logo, subText].forEach {
+            view.addSubview($0)
+        }
+    }
+    
+    override func setLayout() {
+        logo.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.top.equalTo(view.snp.top).offset((bounds.height) / 3.44)
+        }
+        subText.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.top.equalTo(logo.snp.bottom).offset(10)
+        }
+    }
 
 }
