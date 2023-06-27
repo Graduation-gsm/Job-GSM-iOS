fd7c8d9b36929cb64ac459b1ae715012fac4ca23
minaje0917 <k050918@naver.com>
2023-06-27 20:04:33 +0900

    :sparkles: Add :: Insert Info ViewModel

---
+++ job-gsm/Sources/Presentation/InsertInfo/InsertInfoViewModel.swift
@@ -0,0 +1,19 @@
+import Foundation
+import RxFlow
+import RxCocoa
+import RxSwift
+
+class InsertInfoViewModel: BaseViewModel, Stepper{
+
+    struct Input {
+        
+    }
+    
+    struct Output {
+        
+    }
+    
+    func transVC(input: Input) {
+    }
+
+}
