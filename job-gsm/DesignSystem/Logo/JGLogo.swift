ce7f862cc3f3050237250c9a8e020e0531504184
minaje0917 <k050918@naver.com>
2023-06-29 20:12:07 +0900

    :lipstick: UI :: Logo Label Custom View

--- job-gsm/DesignSystem/Logo/JGLogo.swift
+++ job-gsm/DesignSystem/Logo/JGLogo.swift
@@ -1,8 +1,18 @@
-//
-//  JGLogo.swift
-//  job-gsm
-//
-//  Created by 선민재 on 2023/06/29.
-//
-
 import Foundation
+import UIKit
+
+public final class JGLogo: UIImageView {
+    required public init?(coder aDecoder: NSCoder) {
+        super .init(coder: aDecoder)
+    }
+    
+    init(){
+        super.init(frame: CGRect.zero)
+        self.image = UIImage(named: "HomeLogo")
+    }
+//
+//    convenience init() {
+//        self.init()
+//        self.image = UIImage(named: "HomeLogo")
+//    }
+}
