2d99deffcc175c03da64e13af6a8d66b384f147f
minaje0917 <k050918@naver.com>
2023-07-03 17:21:39 +0900

    :sparkles: Add :: BaseURL

---
+++ job-gsm/Sources/Data/General/BaseURL.swift
@@ -0,0 +1,11 @@
+//
+//  BaseURL.swift
+//  job-gsm
+//
+//  Created by 선민재 on 2023/07/03.
+//
+
+import Foundation
+struct BaseURL {
+    static let baseURL = "https://port-0-job-gsm-backend-kvmh2mljm9p2ms.sel4.cloudtype.app/"
+}
