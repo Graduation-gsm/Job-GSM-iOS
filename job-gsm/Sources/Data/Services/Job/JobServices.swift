cb32aab676ccb0e82bf6648e469505890c8194fc
minaje0917 <k050918@naver.com>
2023-07-05 08:55:47 +0900

    :sparkles: Add :: Job Services

---
+++ job-gsm/Sources/Data/Services/Job/JobServices.swift
@@ -0,0 +1,48 @@
+import Foundation
+import Moya
+
+enum JobServices {
+    case homeList(refreshToken: String)
+}
+
+
+extension JobServices: TargetType {
+    public var baseURL: URL {
+        return URL(string: BaseURL.baseURL)!
+    }
+    
+    var path: String {
+        switch self {
+        case .homeList:
+            return "/job/"
+        }
+    }
+    
+    var method: Moya.Method {
+        switch self {
+        case .homeList:
+            return .get
+        }
+    }
+    
+    var sampleData: Data {
+        return "@@".data(using: .utf8)!
+    }
+    
+    var task: Task {
+        switch self {
+        case .homeList:
+            return .requestPlain
+        }
+    }
+    
+    var headers: [String : String]? {
+        switch self {
+        case .homeList(let refreshToken):
+            return["Content-Type" :"application/json", "refreshToken" : refreshToken]
+        default:
+            return["Content-Type" :"application/json"]
+        }
+    }
+}
+
