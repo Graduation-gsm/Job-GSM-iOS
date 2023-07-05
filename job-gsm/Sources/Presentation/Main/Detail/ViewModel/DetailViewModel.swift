1e4fccea65f99301783824e2c457baf36cecf0e5
minaje0917 <k050918@naver.com>
2023-07-05 11:15:19 +0900

    :sparkles: Add :: DetailViewModel

---
+++ job-gsm/Sources/Presentation/Main/Detail/ViewModel/DetailViewModel.swift
@@ -0,0 +1,27 @@
+//
+//  DetailViewModel.swift
+//  job-gsm
+//
+//  Created by 선민재 on 2023/07/05.
+//
+
+import Foundation
+import RxFlow
+import RxCocoa
+import RxSwift
+import Moya
+
+class DetailViewModel: BaseViewModel, Stepper{
+    
+    struct Input {
+        
+    }
+    
+    struct Output {
+        
+    }
+    
+    func transform(input: Input) -> Output{
+        return Output()
+    }
+}
