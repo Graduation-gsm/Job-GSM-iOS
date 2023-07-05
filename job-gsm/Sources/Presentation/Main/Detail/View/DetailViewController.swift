032b9bc155d84afcf91719917dfbb7b78319ec3f
minaje0917 <k050918@naver.com>
2023-07-05 11:17:07 +0900

    :sparkles: Add :: DetailVC

---
+++ job-gsm/Sources/Presentation/Main/Detail/View/DetailViewController.swift
@@ -0,0 +1,18 @@
+import UIKit
+import SnapKit
+import Then
+
+class DetailViewController: BaseViewController<DetailViewModel> {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func addView() {
+        <#code#>
+    }
+    
+    override func setLayout() {
+        <#code#>
+    }
+}
