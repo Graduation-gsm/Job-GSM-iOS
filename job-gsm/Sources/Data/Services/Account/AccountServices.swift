b76ef6ee1dd2455992262af065d5c3cf5912c9e9
minaje0917 <k050918@naver.com>
2023-07-13 13:45:51 +0900

    :sparkles: Add :; Account Services

---
+++ job-gsm/Sources/Data/Services/Account/AccountServices.swift
@@ -0,0 +1,48 @@
+import Foundation
+import Moya
+
+enum AccountServices {
+    case profile(authorization: String)
+}
+
+
+extension AccountServices: TargetType {
+    public var baseURL: URL {
+        return URL(string: BaseURL.baseURL)!
+    }
+    
+    var path: String {
+        switch self {
+        case .profile:
+            return "/account/info"
+        }
+    }
+    
+    var method: Moya.Method {
+        switch self {
+        case .profile:
+            return .get
+        }
+    }
+    
+    var sampleData: Data {
+        return "@@".data(using: .utf8)!
+    }
+    
+    var task: Task {
+        switch self {
+        case .profile:
+            return .requestPlain
+        }
+    }
+    
+    var headers: [String : String]? {
+        switch self {
+        case let .profile(authorization):
+            return["Content-Type" :"application/json", "Authorization" : authorization]
+        default:
+            return["Content-Type" :"application/json"]
+        }
+    }
+}
+
